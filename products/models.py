8b4b6391684c41186e7af283e8115cbde684519a
pedromn0 <muzzioneri@gmail.com>
2022-03-15 19:20:06 +0000

    Finish fixtures and initial product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    """
+    Categroies from fixtures
+    """
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    Products from fixtures
+    """
+
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    sweet_sauvory = models.CharField(max_length=254, null=True, blank=True)
+    included_unity = models.CharField(max_length=254)
+    allergens = models.CharField(max_length=254)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
