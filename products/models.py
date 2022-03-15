a05961ce0c967f3cc00c630d229b088aeb589a52
pedromn0 <muzzioneri@gmail.com>
2022-03-15 19:36:40 +0000

    Customize products and catergory admin

--- products/models.py
+++ products/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
     Categroies from fixtures
     """
 
+    class Meta():
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
