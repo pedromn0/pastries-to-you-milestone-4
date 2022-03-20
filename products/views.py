5391307a74d65e442564c4c54c03fd0f66f07df4
pedromn0 <muzzioneri@gmail.com>
2022-03-20 14:55:32 +0000

    Add sorting option price, category and name through javascript

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.db.models import Q
 from django.contrib import messages
 from .models import Product, Category
+from django.db.models.functions import Lower
 
 # Create your views here.
 
@@ -13,8 +14,24 @@ def all_products(request):
     query = None
     categories = None
     sweet_sauvory_query = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -37,11 +54,14 @@ def all_products(request):
                 name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'sweet_sauvory ': sweet_sauvory_query,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
