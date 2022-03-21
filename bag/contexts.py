d230ac3b2864cfcbe4e9fe2f3846d8328e3d4930
pedromn0 <muzzioneri@gmail.com>
2022-03-21 11:57:05 +0000

    Add main structure for bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -10,12 +12,23 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.DELIVERY_OPTION_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        delivery = 0
         delivery_available_delta = settings.DELIVERY_OPTION_THRESHOLD - total
     else:
-        delivery = 0
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         delivery_available_delta = 0
 
     grand_total = delivery + total
