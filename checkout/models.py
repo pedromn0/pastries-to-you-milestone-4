770012d17397e043febabd9c111a52001d76b062
pedromn0 <muzzioneri@gmail.com>
2022-03-30 21:59:50 +0000

    Add checkout success logic and fix model

--- checkout/models.py
+++ checkout/models.py
@@ -65,15 +65,18 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    """
-    Model responsible for each line of product orders
-    """
-    order = models.ForeignKey(
-        Order, null=False, blank=False,
-        on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(
-        Product, null=False, blank=False, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(
-        max_digits=6, decimal_places=2,
-        null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
