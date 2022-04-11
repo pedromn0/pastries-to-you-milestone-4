da42d11cfbe77f7a507f76e796e314be1d86aa21
pedromn0 <muzzioneri@gmail.com>
2022-04-11 22:53:39 +0000

    Fix typo in settings database

--- pastries_to_you/settings.py
+++ pastries_to_you/settings.py
@@ -121,9 +121,9 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASES_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASES_UR'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
